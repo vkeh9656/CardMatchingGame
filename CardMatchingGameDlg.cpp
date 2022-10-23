2354d0b804064d0dce2d3a664f6e7eb5f9211924
DESKTOP-9FFSOO7\tjdwn <tjdwns9656@naver.com>
2022-10-24 01:04:48 +0900

    그림 이미지 띄우기

--- CardMatchingGameDlg.cpp
+++ CardMatchingGameDlg.cpp
@@ -31,6 +31,8 @@ void CCardMatchingGameDlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(CCardMatchingGameDlg, CDialogEx)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_WM_TIMER()
+	ON_WM_LBUTTONDOWN()
 END_MESSAGE_MAP()
 
 
@@ -45,7 +47,35 @@ BOOL CCardMatchingGameDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
-	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	CString str;
+	for (int i = 0; i < 19; i++)
+	{
+		str.Format(L".\\card_image\\%03d.bmp", i);
+		m_card_image[i].Load(str);
+	}
+
+	for (int i = 0; i < 18; i++)
+	{
+		m_table[i] = i + 1;			// 1 ~ 18, m_table[0] ~ m_table[17]
+		m_table[18 + i] = i + 1;	// 1 ~ 18, m_table[18] ~ m_table[35]
+	}
+
+	srand((unsigned int)time(NULL)); // 난수 시드 설정
+	int first, second, temp;
+	for (int i = 0; i < 100; i++)
+	{
+		first = rand() % 36; // 0 ~ 35
+		second = rand() % 36; // 0 ~ 35
+		if (first != second)
+		{
+			// m_table[first] <--> m_table[second]   SWAP
+			temp = m_table[first];
+			m_table[first] = m_table[second];
+			m_table[second] = temp;
+		}
+	}
+
+	SetTimer(1, 3000, NULL); // NULL을 쓰면 WM_TIMER 메시지를 쓰겠다는 뜻
 
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
@@ -56,10 +86,9 @@ BOOL CCardMatchingGameDlg::OnInitDialog()
 
 void CCardMatchingGameDlg::OnPaint()
 {
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
 	if (IsIconic())
 	{
-		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
-
 		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
 		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
@@ -75,7 +104,25 @@ void CCardMatchingGameDlg::OnPaint()
 	}
 	else
 	{
-		CDialogEx::OnPaint();
+		CString str;
+		int card_index =0;
+		for (int i = 0; i < 36; i++)
+		{
+			if (m_view_flag)
+			{
+				card_index = m_table[i];
+			}
+				
+
+			m_card_image[card_index].Draw(dc, (i % 6) * 36, (i / 6) * 56);
+
+			str.Format(L"%d", m_table[i]);
+			dc.TextOutW(5+(i % 6) * 36, 5+(i / 6) * 56, str);
+		}
+		
+		
+
+		//CDialogEx::OnPaint();
 	}
 }
 
@@ -86,3 +133,24 @@ HCURSOR CCardMatchingGameDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CCardMatchingGameDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	if (nIDEvent == 1)
+	{
+		KillTimer(1);
+		m_view_flag = 0;
+		Invalidate(); // WM_PAINT 메시지 발생 -> OnPaint()
+	}
+
+	CDialogEx::OnTimer(nIDEvent);
+}
+
+
+void CCardMatchingGameDlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
--- CardMatchingGameDlg.h
+++ CardMatchingGameDlg.h
@@ -8,6 +8,10 @@
 // CCardMatchingGameDlg 대화 상자
 class CCardMatchingGameDlg : public CDialogEx
 {
+private:
+	CImage m_card_image[19]; // 0 -> 뒷면, 1 ~ 18 -> 카드사진
+	char m_view_flag = 1;
+	char m_table[36]; // 1차원 배열로 카드판 관리
 // 생성입니다.
 public:
 	CCardMatchingGameDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
@@ -30,4 +34,7 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 };
